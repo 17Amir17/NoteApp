e4ddf375aaff0a4bcaffae899919159fb20edafd
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-02-07 15:38:50 +0200

    0.4.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {
   RichText,
   useEditorBridge,
   CoreBridge,
   TenTapStartKit,
 } from '@10play/tentap-editor';
+import React from 'react';
 const App = () => {
   const editor = useEditorBridge({
-    bridgeExtensions: [CoreBridge, TenTapStartKit],
+    bridgeExtensions: [CoreBridge, ...TenTapStartKit],
     DEV: true,
   });
   return (
